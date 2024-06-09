0a85a2daf0a4ff7bdd9e6655a53d56eadb89ef37
Kawamotus <gustavo.kawamoto@hotmail.com>
2024-06-09 01:02:44 -0300

    arrumando erro na comparacao no petCard

--- src/Components/PetCard.jsx
+++ src/Components/PetCard.jsx
@@ -8,7 +8,7 @@ const PetCard = ({border, name, img, id, sexo}) => {
 
 
   return <>
-    <Card border={border == 1 ? "warning" : border === 2 ? "danger" : "success"} style={{ width: '18rem' }} key={id}>
+    <Card border={border == 1 ? "warning" : border == 2 ? "danger" : "success"} style={{ width: '18rem' }} key={id}>
       <Card.Img variant="top" src={img} style={{objectFit: "cover", height: "300px"}}/>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
