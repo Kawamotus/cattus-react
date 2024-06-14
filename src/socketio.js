737d089cbf54f693b9c8899162f4c29f174958ec
Vinicius Leal Alves <leealalves@hotmail.com>
2024-06-14 20:11:50 -0300

    feature: testes notificacoes de alertas

--- src/socketio.js
+++ src/socketio.js
@@ -3,12 +3,11 @@ import Cookies from 'js-cookie';
 import toast from 'react-hot-toast';
 
 
-const URL = "http://localhost:8080/"
+const URL = `http://localhost:8080/?company=${Cookies.get('company')}`
 
 const socket = new io(URL, {
     extraHeaders: {
-        authorization: Cookies.get("token"),
-        company: Cookies.get("company")
+        authorization: Cookies.get("token")
     },
     reconnectionAttempts: 1
 })
@@ -17,6 +16,15 @@ socket.off("notification")
 socket.on("notification", (info) =>{
     console.log(info);
     toast.success(`${info.notificationOrigin.petBirth}`, {
+        position: 'top-right'
+    });
+    
+})
+
+socket.off("notificationStatus")
+socket.on("notificationStatus", (info) =>{
+    console.log(info);
+    toast.success(`${info.notificationDescription}`, {
         position: 'top-right'
     });
     
